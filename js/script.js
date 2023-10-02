e09a410cce2510f1012d82aa7ba42d3c2a03c54d
yash114bansal <yash114bansal@gmail.com>
2023-10-02 15:34:08 +0530

    fix realtive path of notes

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const allnotes = document.querySelectorAll(".note");
 
 allnotes.forEach(key => {
     key.addEventListener("click", () => {
-        const note = new Audio(`../notes/${key.getAttribute('note')}.mp3`);
+        const note = new Audio(`notes/${key.getAttribute('note')}.mp3`);
         note.play();
     });
 });
